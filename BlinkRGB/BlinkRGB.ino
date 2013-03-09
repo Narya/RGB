5fbdbf8c3eed8fb7c143ad89c445213275d560ab
Narya <github@van-schaik.org>
2013-03-09 18:50:10 +0100

    clean up code

--- BlinkRGB/BlinkRGB.ino
+++ BlinkRGB/BlinkRGB.ino
@@ -1,16 +1,11 @@
 /*
-  Blink RGB
-  Make nice colors
+  Make nice colors with RGB LED
 */
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-// int rood = 15; // analog pin
-// int groen = 16; // analog pin
-// int blauw = 17; // analog pin
-int rood = 9; // digital pin
-int groen = 10; // digital pin
-int blauw = 11; // digital pin
+// define which color is controlled by which pin
+int red = 9; // digital pin
+int green = 10; // digital pin
+int blue = 11; // digital pin
 
 // delay time
 int mytime = 5;
@@ -18,9 +13,9 @@ int mytime = 5;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(rood, OUTPUT);     
-  pinMode(groen, OUTPUT);     
-  pinMode(blauw, OUTPUT);  
+  pinMode(red, OUTPUT);     
+  pinMode(green, OUTPUT);     
+  pinMode(blue, OUTPUT);  
   Serial.begin(9600);
   switchOff();
 }
@@ -33,25 +28,21 @@ void loop() {
 
 void switchOn(int r,int g,int b) {
   // reverse pin output (0 is high, 255 is low with a combined anode)
-//  Serial.print(r);
-//  Serial.print(" ");
-//  Serial.print(g);
-//  Serial.print(" ");
-//  Serial.println(b);
   r = 255 - r;
   g = 255 - g;
   b = 255 - b;
   // change the colors
-  analogWrite(rood, r);   // turn the LED on with LOW
-  analogWrite(groen, g);   // turn the LED on with LOW
-  analogWrite(blauw, b);   // turn the LED on with LOW
-  delay(mytime);               // wait for some time
+  analogWrite(red, r);
+  analogWrite(green, g);
+  analogWrite(blue, b);
+  delay(mytime); // wait for some time
 }
 
 void switchOff() {
-  analogWrite(rood, 255);   // turn the LED off (HIGH is the voltage level)
-  analogWrite(groen, 255);   // turn the LED off (HIGH is the voltage level)
-  analogWrite(blauw, 255);   // turn the LED off (HIGH is the voltage level)
+  // turn the LED off (high nr is off in case of common anode)
+  analogWrite(red, 255);
+  analogWrite(green, 255);
+  analogWrite(blue, 255);
 }
 
 void purpleFade() {
